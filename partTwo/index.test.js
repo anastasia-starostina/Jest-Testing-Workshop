0f05d17c3fb9c27f5226da76f4a4f32710523912
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-05-30 21:06:10 +0100

    Part Two all Tests Complete

--- partTwo/index.test.js
+++ partTwo/index.test.js
@@ -66,3 +66,40 @@ test(`Given one array, if its length exceeds 10, return true, if it is less than
 })
 
 //Add Item to Array 
+test(`Given a string and an array, addItemToArray returns a new array that now includes that string.`, () => {
+  //ARRANGE
+  const item = 'pancakes'
+  const array = ['waffles', 'french toast', 'cereal'];
+  const expected = ['waffles', 'french toast', 'cereal', 'pancakes'];
+  // ACT
+  const actual = addItemToArray(array, item);
+  //ASSERT
+  expect(actual).toStrictEqual(expected);
+} )
+
+//addKeyValuePairToObject Test 
+
+test(`Given an object, key and value, addKeyValuePairToObject returns an object with the key-value pair in it.`, () => {
+  //ARRANGE
+  const key = 'fruit'
+  const value = 'kiwi'
+  const object = {}
+  const expected = {'fruit': 'kiwi'}
+  //ACT 
+  const actual = addKeyValuePairToObject(object, key, value)
+  //ASSERT
+  expect(actual).toStrictEqual(expected);
+})
+
+//throwErrorIfEven Test 
+test(`Given one number, if number is even, throw an error that returns a string 'Error, given number is even!'`, () => {
+  //ARRANGE
+  const number = 3;
+  const expected = false;
+
+  //ACT 
+  const actual = throwErrorIfEven(number)
+
+  //ASSERT
+  expect(actual).toBe(expected)
+})
